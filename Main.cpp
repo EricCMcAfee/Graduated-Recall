7b376fe504fd9359312d43e58ed54885e71dad9c
EricCMcAfee <eric.c.mcafee@gmail.com>
2024-07-29 16:15:23 -0600

    added join function

--- Main.cpp
+++ Main.cpp
@@ -10,22 +10,21 @@ VocabList WEEKWORDS;
 
 int main()
 {
-    VocabList myList;
-    myList.Append(myList.InitWord("one", "eins"));
-    myList.Append(myList.InitWord("two", "zwei"));
-    myList.Append(myList.InitWord("three", "drei"));
-    myList.Append(myList.InitWord("four", "vier"));
-    myList.Append(myList.InitWord("five", "funf"));
+    VocabList* myList = new VocabList();
+    myList->Append(myList->InitWord("one", "eins"));
+    myList->Append(myList->InitWord("two", "zwei"));
+    myList->Append(myList->InitWord("three", "drei"));
+    myList->Append(myList->InitWord("four", "vier"));
+    myList->Append(myList->InitWord("five", "funf"));
 
-    myList.Append(myList.InitWord("masculine definite article", "der"));
-    myList.Append(myList.InitWord("feminine definite article", "die"));
-    myList.Append(myList.InitWord("neuter definite article", "das"));
+    myList->Append(myList->InitWord("masculine definite article", "der"));
+    myList->Append(myList->InitWord("feminine definite article", "die"));
+    myList->Append(myList->InitWord("neuter definite article", "das"));
 
-    myList.Append(myList.InitWord("red", "rot"));
-    myList.Append(myList.InitWord("yellow", "gelb"));
-    myList.Append(myList.InitWord("blue", "blau"));
-    myList.Append(myList.InitWord("black", "schwarz"));
-    myList.Append(myList.InitWord("white", "weiss"));
+    myList->Append(myList->InitWord("red", "rot"));
+    myList->Append(myList->InitWord("yellow", "gelb"));
+    myList->Append(myList->InitWord("blue", "blau"));
+    myList->Append(myList->InitWord("black", "schwarz"));
+    myList->Append(myList->InitWord("white", "weiss"));
 
-    myList.Print();
 }
